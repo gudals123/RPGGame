f833919b5aa69b060e6e79b5aabd73b08534d66f
gudals123 <wnsals2111@naver.com>
2024-04-18 17:51:56 +0900

    프로젝트 추가

---
+++ Assets/Script/Entity.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
